b534a53212d3b1f785aea3235defb8f94c5d1cff
sebastien.leprovost <sebastien@akki.fr>
2023-02-15 18:56:05 +0100

    RM-982

---
+++ src/Extension/FromForeignGatewayExtension.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Akki\SyliusPayumPayzenPlugin\Extension;
+
+use Akki\SyliusPayumPayzenPlugin\Action\ConvertPaymentAction;
+use Payum\Core\Extension\Context;
+use Payum\Core\Extension\ExtensionInterface;
+use Payum\Core\Request\Convert;
+use Sylius\Component\Core\Model\PaymentInterface;
+
+final class FromForeignGatewayExtension implements ExtensionInterface
+{
+
+    public const VADS = "vads_";
+
+    public function onPreExecute(Context $context): void
+    {
+    }
+
+    public function onExecute(Context $context): void
+    {
+        if (null !== $context->getException()) {
+            return;
+        }
+
+        $action = $context->getAction();
+        if (false === $action instanceof ConvertPaymentAction) {
+            return;
+        }
+
+        $request = $context->getRequest();
+        if (false === $request instanceof Convert) {
+            return;
+        }
+
+        $paiement = $request->getSource();
+        if (false === $paiement instanceof PaymentInterface) {
+            return;
+        }
+
+        $details = $paiement->getDetails();
+        if (false === is_array($details)) {
+            return;
+        }
+
+        $found = false;
+        foreach ($details as $key => $value) {
+            if (0 !== strpos($key, self::VADS) ) {
+                $found = true;
+            }
+        }
+
+        // Reset the details we are coming from a foreign gateway
+        if (true === $found) {
+            $paiement->setDetails([]);
+        }
+    }
+
+    public function onPostExecute(Context $context): void
+    {
+    }
+}
