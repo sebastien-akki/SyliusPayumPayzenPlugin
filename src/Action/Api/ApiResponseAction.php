262acf01ace1854005b72da7b4e3baf7c8c1523c
sebastien.leprovost <sebastien@akki.fr>
2023-11-16 17:03:24 +0100

    RM-1261

--- src/Action/Api/ApiResponseAction.php
+++ src/Action/Api/ApiResponseAction.php
@@ -21,48 +21,6 @@ class ApiResponseAction extends AbstractApiAction
     {
         /** @var Response $request */
         RequestNotSupportedException::assertSupports($this, $request);
-
-        $model = ArrayObject::ensureArrayObject($request->getModel());
-
-        $this->gateway->execute($httpRequest = new GetHttpRequest());
-
-        if (isset($httpRequest->request['vads_result'])) {
-            $data = $httpRequest->request;
-        } elseif (isset($httpRequest->query['vads_result'])) {
-            $data = $httpRequest->query;
-        } else {
-            return;
-        }
-
-        $this->logResponseData($data);
-
-        // Check the response signature
-        if ($this->api->checkResponseIntegrity($data)) {
-            // Update the payment details
-            $model->replace($data);
-            $request->setModel($model);
-        }
-    }
-
-    /**
-     * Logs the response data.
-     *
-     * @param array $data
-     */
-    private function logResponseData(array $data): void
-    {
-        $this->logData("[Payzen] Response", $data, [
-            'vads_order_id',
-            'vads_trans_id',
-            'vads_amount',
-            'vads_auth_result',
-            'vads_auth_mode',
-            'vads_auth_number',
-            'vads_validation_mode',
-            'vads_result',
-            'vads_extra_result',
-            'vads_warranty_result',
-        ]);
     }
 
     /**
